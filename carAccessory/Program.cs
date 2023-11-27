db97b245e936713948e8efcc308ffa69d2e840a2
comus3 <come.plantin.carrenard@gmail.com>
2023-11-27 10:38:43 +0100

    getting there

--- carAccessory/Program.cs
+++ carAccessory/Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+namespace carAccessory;
+class Program
+{
+
+    static void Main(string[] args)
+        {
+        //Instancier l'objet myDbConn en lui donnant la chaine de connexion
+        //myDbConn= new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\bus.accdb");
+        MySqlConnection myDbConn = new MySqlConnection(@"server=127.0.0.1;userid=root;pwd=root;persistsecurityinfo=True;database=dvauto;port=3308;sharedmemoryname=");
+        //Construiser la chaine de connexion dans VS / Server explorer / Connect to database
+        // Ouverture et fermeture de la connexion
+        try
+        {
+            // open connection
+            myDbConn.Open();
+            // Commande SQL SELECT
+            // -------------------
+            String myQueryString = "SELECT * FROM tb_fournisseur ";
+            // declare oleDbcommand object
+            MySqlCommand myCmd = new MySqlCommand(myQueryString, myDbConn);
+            /*
+            // declare oleDbcommand object Version 2 ;-)
+            OleDbCommand myCmd = new OleDbCommand();
+            cmd.Connection = myDbConn;
+            cmd.CommandText = myQueryString;
+            */
+            //OleDbDataReader permet de recevoir le résultat d'une requête SELECT
+            MySqlDataReader myDbReader = null;
+            // execute SQL Command
+            myDbReader = myCmd.ExecuteReader();
+            /*
+            // Show first row first col ([0])
+            myDbReader.Read();
+            MessageBox.Show(myDbReader[0].ToString());
+            // read each record
+            while (myDbReader.Read()) // read next line
+            {
+            // extract data from record
+            string ipk_Fourn = myDbReader["pk_Numero_fournisseur"].ToString();
+            string nom = myDbReader.GetString(1);
+            string adresse = myDbReader.GetString(2);
+            // do something with data ...
+            }
+            */
+            // pour plus de souplesse, on peut stocker le dbReader dans une DataTable
+            DataTable myDT = new DataTable();
+            // save query results in the datatable
+            myDT.Load(myDbReader);
+            // Show first row, field "Nom"
+            //MessageBox.Show(myDT.Rows[0]["Nom"].ToString());
+            // If there is a dataGridView on the WinForm :
+            /*
+            // Commande SQL INSERT
+            //--------------------
+            myQueryString = "insert into cepage (nom) VALUES ('" + tb_cepage.Text + "')
+            ";
+            // declare oleDbcommand object
+            OleDbCommand myInsertCmd = new OleDbCommand(myQueryString, myDbConn);
+            // execute insert command
+            int nb_row = myInsertCmd.ExecuteNonQuery();
+            if (nb_row == 1)
+            MessageBox.Show("Enregistrement réussi ");
+            // Même logique pour UPDATE ou DELETE (requêtes SQL sans autre retour que
+            le nombre de lignes affectées
+            */
+        }
+        catch (Exception ex)
+        {
+            //MessageBox.Show("Failed to connect to data source " + ex).ToString();
+        }
+        finally
+        {
+            // Disconnect Database
+            myDbConn.Close();
+        }
+    }
+
+}
+
---
+++ carAccessory/Terminal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+namespace carAccessory;
+
+class Terminal
+{
+    private Dictionary<string, Action> switcher;
+    
+    public void terminal()
+    {
+        // Initialisez vos données ici (fournisseurs, articles, etc.)
+        switcher = new Dictionary<string, Action>
+        {
+            { "afficherTabs", AfficherListes },
+            { "afficherArticles", AfficherArticles },
+            { "afficherArticlesCategories", AfficherArticlesParCategorie },
+            { "AfficherArticlesVendusAChampion", AfficherArticlesVendusAChampion },
+            { "ArticlesStockes", AfficherFournisseursVolantStockSup5 },
+            { "a_calc", AfficherFournisseursMoinsChersParArticle },
+            { "b_calc", ListePrixMoyensParProduit },
+            {"help",Help},
+            { "exit", Exit }
+        };
+    }
+
+        public void SwitchCase(string commande)
+    {
+        if (switcher.TryGetValue(commande, out var action))
+        {
+            action();
+        }
+        else
+        {
+            Console.WriteLine("Commande invalide");
+        }
+    }
+
+    static void Help()
+    {
+        Console.WriteLine("Bienvenue dans l'aide de l'application:");
+        Console.WriteLine();
+        Console.WriteLine("Commandes disponibles :");
+        Console.WriteLine(" - afficherTabs : Affiche les onglets (tables) disponibles.");
+        Console.WriteLine(" - afficherArticles : Affiche la liste des articles.");
+        Console.WriteLine(" - afficherArticlesCategories : Affiche la liste des articles par catégorie.");
+        Console.WriteLine(" - AfficherArticlesVendusAChampion : Affiche les articles vendus à Champion.");
+        Console.WriteLine(" - ArticlesStockes : Affiche les articles stockés avec plus de 5 pièces.");
+        Console.WriteLine(" - a_calc : Affiche les fournisseurs les moins chers pour chaque article.");
+        Console.WriteLine(" - b_calc : Affiche la liste des prix moyens pour chaque produit.");
+        Console.WriteLine(" - help : Affiche cette aide.");
+        Console.WriteLine();
+        Console.WriteLine("Utilisez 'exit' pour quitter l'application.");
+    }
+
+    private void Exit()
+    {
+        Environment.Exit(0);
+    }
+
+    public void TerminalRunner()
+    {
+        var app = new Terminal();
+            while (true)
+            {
+                Console.Write("Entrez une commande ");
+                var commande = Console.ReadLine().ToLower();
+                app.SwitchCase(commande);
+            }
+    }
+}
+
+}

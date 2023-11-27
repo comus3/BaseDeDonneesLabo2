48e5edfc96d1c154d72e86bfca949acbe684ee72
comus3 <come.plantin.carrenard@gmail.com>
2023-11-27 11:18:12 +0100

    display method

---
+++ carAccessory/Displayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+namespace carAccessory;
+
+public class Displayer
+{
+     static void DisplayData(DataTable dataTable)
+    {
+        // Display column headers
+        foreach (DataColumn column in dataTable.Columns)
+        {
+            Console.Write($"{column.ColumnName,-20}");
+        }
+        Console.WriteLine();
+
+        // Display data rows
+        foreach (DataRow row in dataTable.Rows)
+        {
+            foreach (DataColumn column in dataTable.Columns)
+            {
+                Console.Write($"{row[column],-20}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
